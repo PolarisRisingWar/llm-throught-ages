d5f055c54075b7a222d10a999d43e77077ed8649
PolarisRisingWar <1499695895@qq.com>
2023-06-08 12:58:48 +0800

    增加代码中的开源许可证和LLaMA的README文件

--- models/BERT/TC/bert_tc_native_pytorch.py
+++ models/BERT/TC/bert_tc_native_pytorch.py
@@ -1,4 +1,14 @@
-#基于PyTorch 1.8.1，transformers 4.18.0，datasets 2构建
+"""
+   Copyright (c) [2023] [Wang Huijuan]
+   [LLM Throughtout Ages] is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2. 
+   You may obtain a copy of Mulan PSL v2 at:
+               http://license.coscl.org.cn/MulanPSL2 
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
+   See the Mulan PSL v2 for more details.  
+   
+基于PyTorch 1.8.1，transformers 4.18.0，datasets 2构建
+"""
 
 from tqdm.auto import tqdm
 
---
+++ models/LLaMA/README.md
@@ -0,0 +1,3 @@
+LLaMA huggingface.transformers官网：<https://huggingface.co/docs/transformers/main/model_doc/llama>
+
+[ymcui/Chinese-LLaMA-Alpaca: 中文LLaMA&Alpaca大语言模型+本地CPU/GPU训练部署 (Chinese LLaMA & Alpaca LLMs)](https://github.com/ymcui/Chinese-LLaMA-Alpaca)
